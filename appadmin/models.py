e9c9cf5e39d7ca38f2024844781c3dc49094d911
sachu <seartsachu@gmail.com>
2020-07-26 09:29:20 +0530

    commit

--- appadmin/models.py
+++ appadmin/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class CustomDateTimeField(models.DateTimeField):
     def value_to_string(self, obj):
-        val = self.value_from_object(obj)pip
+        val = self.value_from_object(obj)
         if val:
             val.replace(microsecond=0)
             return val.isoformat()
@@ -14,7 +14,8 @@ class Business_category(models.Model):
     name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=False)
     people_involve = models.IntegerField(default=0)
-    created = CustomDateTimeField(auto_now_add=True)
+    account_creation = CustomDateTimeField(auto_now_add=True)
     def __str__(self):
-        st = "%s  -  %s" % (self.id, self.name)
+        st = "%s  -   %s" % (self.id, self.name)
         return st
+
