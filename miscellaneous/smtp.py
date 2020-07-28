a3e8a20320691b7268392f1e5fab27c02a2fbc69
sachu <seartsachu@gmail.com>
2020-07-29 00:24:46 +0530

    user_creation

--- miscellaneous/smtp.py
+++ miscellaneous/smtp.py
@@ -1,5 +1,5 @@
 import smtplib
-
+from django.shortcuts import HttpResponse
 def smtp(name,l,otp,email):
     msg = "--*---*------WELCOME TO OUR HandyMan -----*---*--\n\n\n" \
           "          HI,%s \n\n" \
@@ -9,8 +9,14 @@ def smtp(name,l,otp,email):
           "             LINK   : %s \n\n" \
           "          LINK GENERATION TIME      : %s \n\n" % (name, l, str(otp))
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login('godhelpmetogrow@gmail.com', 'sachu123@')
-    server.sendmail('godhelpmetogrow@gmail.com', email, msg)
-    server.quit()
+    # server = smtplib.SMTP("smtp.gmail.com", 587)
+    # server.starttls()
+    # server.login('godhelpmetogrow@gmail.com', 'sachu123@')
+    # server.sendmail('godhelpmetogrow@gmail.com', email, msg)
+    # server.quit()
+    # ('mail.your-domain.com', 25)
+    try:
+       smtpObj = smtplib.SMTP('mail.https://guarded-thicket-09826.herokuapp.com', 25)
+       smtpObj.sendmail('godhelpmetogrow@gmail.com', email, msg)
+    except smtplib.SMTPException:
+       return HttpResponse("<h1>Error: unable to send email</h1>")
